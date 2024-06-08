25c9f394115f538d5114899d46a25424e9e35937
Matias Vicuña Cofré <matiasvicunacofre@gmail.com>
2024-06-07 23:36:25 -0400

    Actualizacion Modelo - Generacion Script Matlab
    Generamos el Script de MATLAB para inicializar los modelos de Dynares.

---
+++ Inicializador_Simulaciones_Dynare.m
@@ -0,0 +1,12 @@
+%% Primer Modelo: Hansen - Wright
+% Usando el paper de Hansen y Wright (1992), replicamos el modelo.
+
+dynare Modelo_Benchmark.mod
+
+
+%% Segundo Modelo
+
+
+%% Tercer Modelo
+
+
