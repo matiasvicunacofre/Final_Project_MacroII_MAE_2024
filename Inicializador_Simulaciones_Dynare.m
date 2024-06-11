b7e35d533ca25dfa34d58e5f83321dce4cdb04ed
Matias Vicuña Cofré <matiasvicunacofre@gmail.com>
2024-06-11 13:19:41 -0400

    Reorganización Archivos y Modificando Scripts

---
+++ Inicializador_Simulaciones_Dynare.m
@@ -0,0 +1,20 @@
+%% Inicializador de Modelos
+% Autores: Matías Espinoza, Kevin Alvarez, Matías Vicuña
+% Macroeconomía II - Magíster en Economía
+% Universidad Alberto Hurtado
+
+%% Primera Extensión Modelo Hansen - Wright (1992): Modelo Trabajo Indivisible
+% Usando el paper de Hansen y Wright, replicamos la primera extensión.
+% función "get_simul_replications.m" de Johannes Pfeifer
+clearvars; close all; clc;
+
+% Comenzamos definiendo el espacio de los modelo de dynare (.mod)
+dynare BaseModel.mod;
+
+%% Segunda Extensión Modelo Hansen - Wright (1992)
+% Usando el paper de Hansen y Wright, replicamos la segunda extensión.
+% función "get_simul_replications.m" de Johannes Pfeifer
+clearvars; close all; clc;
+
+% Comenzamos definiendo el espacio de los modelo de dynare (.mod)
+dynare BaseModel.mod;
